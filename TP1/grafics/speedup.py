8a03efadc2ae1ef49aa8cffbeac7114001737013
lucianobajr <luciano.alcantara@ufv.br>
2020-10-03 10:26:57 -0300

    add tp2 docs

--- TP1/grafics/speedup.py
+++ TP1/grafics/speedup.py
@@ -6,8 +6,8 @@ values = [140.9, 128.8, 29.29]
 xs = [i + 0.1 for i, _ in enumerate(computers)]
 # as barras do gráfico com as coordenadas x à esquerda [xs], alturas [values]
 plt.bar(xs, values)
-plt.ylabel("#  Novo tempo de Resposta")
-plt.title("Computador x Novo Tempo de Resposta")
+plt.ylabel("# Tempo de Resposta")
+plt.title("Computador x Tempo de Resposta")
 # nomeia o eixo x
 plt.xticks([i + 0.5 for i, _ in enumerate(computers)], computers)
 plt.show()
