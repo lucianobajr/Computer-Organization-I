11e1e93637e7f0f7fde34f5d06560d879c791750
lucianobajr <luciano.alcantara@ufv.br>
2020-10-18 20:08:29 -0300

    some changes

--- TP2/src/main.py
+++ TP2/src/main.py
@@ -1,37 +1,51 @@
 import sys
 import json
 
+#----------------------JSONS--------------------------
+
+'''
+    lê o json com as instruções do risc-v
+'''
 with open('data/instructions.json', 'r') as json_file:
     instructions = json.load(json_file)
 
+'''
+    lê o json com os registradores do risc-v
+'''
 with open('data/registers.json', 'r') as json_file:
     registers = json.load(json_file)    
+    
+#----------------------JSONS--------------------------
+
+
+#--------------------FUNCTIONS----------------------
 
 '''
     Adiciona 0's até o comando possuir o 
     seu devido número de bits
- '''
-def add_zero(binStr, size):  
-    while len(binStr) < size:
-        binStr = "0" + binStr
-    return binStr
+'''
+def add_zero(binary, number_command_bits):
+    while len(binary) < number_command_bits:
+        binary = "0" + binary
+    return binary
+
 
 '''
     transforma o valor correspondente do 
     registrador em decimal para binário
- '''
+'''
 def change_reg_to_bin(register):
-    return str(bin(registers[register]))[2:]  # apatir da 2 posição do string
+    return str(bin(registers[register]))[2:]  # apatir da 2 posição do string -> retirando 0b
+
 
 ''' Torna código assembly em binário'''
-
-def assembler(statement):
-    command = statement[: statement.find(" ")]  # até o espaço
-    regs = statement[statement.find(" "):].split(",")
+def assembler(file_line):
+    command = file_line[: file_line.find(" ")]  #lê a linha até o  primeiro espaço
+    regs = file_line[file_line.find(" "):].split(",")
     for i in range(len(regs)):
         regs[i] = regs[i].strip() # retira o espaço e o '\n' de cada registrador
 
-    machine_code = ""
+    binary_result = ""
 
     if instructions[command]['type'] == 'r':
         f7 = instructions[command]['funct7']
@@ -39,44 +53,58 @@ def assembler(statement):
         rs1 = change_reg_to_bin(regs[1]) # segundo reg
         rd = change_reg_to_bin(regs[0])  # primeiro reg
         f3 = instructions[command]['funct3']
-        opcode = instructions[command]["opcode"]
+        opcode = instructions[command]['opcode']
+
+        ''' 
+            rs2,rs1 e rd podem ainda não estar com o 
+                tamanho correto de bits 
+        '''
 
         rs2 = add_zero(rs2, 5)
         rs1 = add_zero(rs1, 5)
         rd = add_zero(rd, 5)
 
-        machine_code = f7 + rs2 + rs1 + f3 + rd + opcode
-
+        binary_result = f7 + rs2 + rs1 + f3 + rd + opcode
 
     elif instructions[command]['type'] == 'i':
-        immd = regs[2] # valor imediato
+        immediate = regs[2] # valor imediato
         ''' 
         - & = and bit a bit 
         - reseta um ou mais bits sem afetar o estado dos demais 
-        - operação i -> complemento de dois 
+        - operação & -> complemento de dois 
         '''
-        immd = (bin(int(immd) & 0b111111111111))
-        immd = immd.replace("0b", "") # retira valores da transformação para binário
+        immediate = (bin(int(immediate) & 0b111111111111))
+        immediate = immediate.replace('0b', "") # retira valores da transformação para binário
         
         rs1 = change_reg_to_bin(regs[1])
-        f3 = instructions[command]["funct3"]
+        f3 = instructions[command]['funct3']
         rd = change_reg_to_bin(regs[0])
-        opcode = instructions[command]["opcode"]
+        opcode = instructions[command]['opcode']
 
-        immd = add_zero(immd, 12)
+        immediate = add_zero(immediate, 12)
         rs1 = add_zero(rs1, 5)
         rd = add_zero(rd, 5)
 
-        machine_code = immd + rs1 + f3 + rd + opcode
+        binary_result = immediate + rs1 + f3 + rd + opcode
 
-    return machine_code
+
+    return binary_result
+
+def print_output():
+    print('\n')
+    print('-----------  OUTPUT  -----------')
+    print('\n')
+
+#--------------------FUNCTIONS----------------------
 
 if __name__ == "__main__":
-    with open(str(sys.argv[2]), "r") as f: #valor de input do
-        arq = open(str(sys.argv[3]), "w")
+    with open(str(sys.argv[2]), 'r') as file_input: # arquivo de input
+        output = open(str(sys.argv[3]), 'w')   # arquivo de output
+        print_output()
         while True:
-            statement = f.readline()
-            if not statement:
+            file_line = file_input.readline()
+            if not file_line:
+                print_output()
                 break
-            arq.write(assembler(statement)+'\n')
-            print(assembler(statement))
+            output.write(assembler(file_line)+'\n')
+            print(assembler(file_line))
