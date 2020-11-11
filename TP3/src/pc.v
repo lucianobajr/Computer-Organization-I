470a277817ec167fea72c80588ac14ff5a23155d
BloodSymphony <70582558+BloodSymphony@users.noreply.github.com>
2020-11-11 19:23:07 -0300

    contador

--- TP3/src/pc.v
+++ TP3/src/pc.v
@@ -1,5 +1,9 @@
-module pc;
-
+module Program_counter (in_count,end_count,clock);
+  input clock;
+  input [31:0] in_count;
+  output reg[31:0] end_count;
+  always @ (posedge clock) begin
+    end_count = in_count;
+  end
 endmodule
-
-// pc
+//pc
