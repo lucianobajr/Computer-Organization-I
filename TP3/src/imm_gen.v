68735a3fbb90a7dd0d88aaec807b1ac9e892feea
PapaArt <48726318+PapaArt@users.noreply.github.com>
2020-11-11 19:40:16 -0300

    Update imm_gen.v

--- TP3/src/imm_gen.v
+++ TP3/src/imm_gen.v
@@ -1,5 +1,12 @@
-module imm_gen;
-
+module extensao_sinal(
+    input wire [15:0] in,
+    output reg [31:0] extensor
+);
+    
+always @(*) 
+    begin
+        extensor <= {{16{in[15]}},in};   
+    end
 endmodule
 
-// imm_gen
+// extensao_sinal
