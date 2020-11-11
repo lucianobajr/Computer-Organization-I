094a2ed4f4972acdf3e84e2c2d75d8c67a789568
BloodSymphony <70582558+BloodSymphony@users.noreply.github.com>
2020-11-11 16:58:30 -0300

    datamemory

--- TP3/src/data_memory.v
+++ TP3/src/data_memory.v
@@ -1,5 +1,10 @@
-module data_memory;
-
+module data_Memory(clock, write_Enable, address, write_Data, read_Data);
+   input clock, write_Enable;
+   input[5:0] address;
+   input[31:0] write_Data;
+   output[31:0] read_Data;
+   reg [31:0] RAM2[0:63];
+   assign read_Data = RAM2[address[5:2]];
+   always @(posedge clock)
+   if (write_Enable) RAM2[address[5:2]] <= write_Data;
 endmodule
-
-// data_memory
