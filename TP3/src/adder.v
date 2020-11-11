e1cce5719c46d068cd67d2b655572ede4eeb9594
BloodSymphony <70582558+BloodSymphony@users.noreply.github.com>
2020-11-11 16:48:16 -0300

    adder

--- TP3/src/adder.v
+++ TP3/src/adder.v
@@ -1,5 +1,19 @@
-module adder;
+module adder(a, b,  out);
+   input[31:0] a, b;
+   output[31:0] out;
+   assign out = OUT(a, b);
 
+   function [31:0] OUT;
+       input [31:0] a, b;
+       begin
+       casex(b[31])
+           1'b1:   begin
+                   b = ~b;
+                   b = b + 1'b1;
+                   OUT = a - b;
+                   end
+           default: OUT = a + b;
+       endcase
+     end
+   endfunction
 endmodule
-
-// adder
