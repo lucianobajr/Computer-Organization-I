fa0fee96c8bdde4276acf59121733f00ad56280c
PapaArt <48726318+PapaArt@users.noreply.github.com>
2020-11-16 17:13:56 -0300

    Update pc.v

--- TP3/src/pc.v
+++ TP3/src/pc.v
@@ -1,13 +1,3 @@
-// module program_counter (in_count,end_count,clock);
-//   input clock;
-//   input [31:0] in_count;
-//   output reg[31:0] end_count;
-//   always @ (posedge clock) begin
-//     end_count = in_count;
-//   end
-// endmodule
-// //pc
-
 module program_counter (pc, reset, pc_next);
   input wire reset;
   output reg [31:0] pc;
