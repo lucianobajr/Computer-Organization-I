c96da7fc675651b807c665d3e014191a823c0efd
PapaArt <48726318+PapaArt@users.noreply.github.com>
2020-11-16 17:13:14 -0300

    Update instruction_memory.v

--- TP3/src/instruction_memory.v
+++ TP3/src/instruction_memory.v
@@ -1,14 +1,3 @@
-// module intruction_memory(address, read_data);
-//     input [5:0] address;
-//     output [31:0] read_data;
-//     reg [31:0] RAM[0:63];
-
-//     initial $readmemh("instructions.bin", RAM);
-//     assign read_data = RAM[address];
-// endmodule
-
-// // intruction_memory --> LUCIANO
-
 module InstructionMemory (ReadAddress, Instruction);
   input wire [31:0]ReadAddress;
   output reg [31:0]Instruction;
